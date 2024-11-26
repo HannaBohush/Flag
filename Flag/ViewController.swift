7c20e353f8e919b368a0e50757314bfc4abaa1ec
HannaBohush <annbogush198@gmail.com>
2024-11-26 17:49:11 +0100

    Initial Commit

---
+++ Flag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flag
+//
+//  Created by Анна Богуш on 26/11/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
